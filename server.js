52fb6432474e13e823ff02b3df5a8f5495d0358c
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-06-17 09:31:23 -0500

    Start placing HTML and API routes

--- server.js
+++ server.js
@@ -2,4 +2,29 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.json(__dirname, "../db/db.json"))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
